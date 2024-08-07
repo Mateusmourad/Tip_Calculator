70ebbce1b735bf6008774b64531b2df9e46d43b7
Mateusmourad <mateusmouraduarte@hotmail.com>
2024-08-06 22:57:53 -0300

    Utilizando findViewBylD e ViewBinding

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity: AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClean.setOnClickListener {
+            println("Mateus1"+ binding.tieTotal.text)
+            println("Mateus1"+ binding.tieNumPeople.text)
+        }
     }
 
 }
+
+
 
 
 
